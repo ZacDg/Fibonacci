31b61dfd13ddecb12b51848b0b0a213c688950a8
Manasses <manassesb89@gmail.com>
2022-09-21 21:37:56 -0300

    1.Alterações:Fib.c funcionando com laço FOR; 2.Acréscimo: executável através de make e Makefile para Fib.c rodar através do terminal

--- Fib.c
+++ Fib.c
@@ -1,30 +1,44 @@
 
 #include <stdio.h>
 
-// Funcao recursiva que recebe um inteiro n >= 1
+// Funcao com laço FOR que recebe um inteiro n >= 1
 // Retorna o n-esimo termo da sequencia de Fibonacci.
 
 int fibonacci(int n) {
-	 int x;
+ int x, n1, n2, contador;
+	 contador = 0;
+	 n1=0;
+	 n2=1;
 
-	 if (n == 1) {
-  	 return(1);
+	if (n == 1) {
+		printf("\n\n\n\n\n Sequência de Fibonacci com %d elemento  >>>   ", n);
+  	    printf(" %d", 1);
 	}
 
-	if (n == 2) {
-	return(1);
+	else if (n == 2) {
+		printf("\n\n\n\n\n Sequência de Fibonacci com %d elemento  >>>    ", n);
+	    printf(" %d  %d", 1, 1);
 	}
-
-	x = fibonacci(n-1) + fibonacci(n-2);
-
-	return(x);
+	
+    else if (n >= 3) {        
+		printf("\n\n\n\n\n Sequência de Fibonacci com %d elementos >>> ", n);
+		printf("  %d", 1);
+        for(contador = 1; contador <= (n-1); contador++){
+            x = n2 + n1;
+            printf(" %d", x);
+            n1 = n2;
+            n2 = x;      
+            }    
+        }
+printf("\n\n\n See you later...\n\n\n\n\n");
+return 0;	
 }
 
 
 int main() {
 	 int n,i;
 
-	printf("Deseja que sejam exibidos quantos  termos da sequencia?");
+	printf("Deseja que sejam exibidos quantos  termos da sequencia?  >  ");
 	scanf("%d", &n);
 
 	while(n <= 0) {
@@ -32,9 +46,8 @@ int main() {
 		scanf("%d", &n);
 	}
 
-	for (i = 1; i <= n; i++) {
-		printf("%d ", fibonacci(i));
-	}
-	printf("\n See you later...\n");
+	
+	printf("%d ", fibonacci(n));
+	
 	return(0);
 }
